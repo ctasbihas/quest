1c7f605e1f32f2d568013d6cb0329f5120fa9065
ctasbihas <ctasbihas@gmail.com>
2024-04-23 19:48:37 +0600

    sign up api created

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,44 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
 import express, { Request, Response } from 'express';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
+const prisma = new PrismaClient();
+
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
 	res.send('Hello, World!');
 });
-app.get('/api', async (req: Request, res: Response) => {
-	res.json({ message: 'Hello, World!' });
+
+// User API routes
+app.post('/createUser', async (req: Request, res: Response) => {
+	const { name, email, password } = req.body;
+
+	if (!name || !email || !password) {
+		return res.status(400).json({ error: 'Missing Info' });
+	}
+	const userExists = await prisma.user.findUnique({
+		where: {
+			email,
+		},
+	});
+	if (userExists) {
+		return res.status(400).json({ error: 'User already exists' });
+	}
+
+	const hashedPassword = await bcrypt.hash(password, 10);
+
+	const user = await prisma.user.create({
+		data: {
+			name,
+			email,
+			password: hashedPassword,
+		},
+	});
+
+	res.send(user);
 });
 
 app.listen(port, () => {
