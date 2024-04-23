04fb9866117b6edfc4ea447a7d2eb1e8fcf07246
ctasbihas <ctasbihas@gmail.com>
2024-04-23 12:16:02 +0600

    test

--- server/index.ts
+++ server/index.ts
@@ -6,6 +6,9 @@ const port = process.env.PORT || 3000;
 app.get('/', (req: Request, res: Response) => {
 	res.send('Hello, World!');
 });
+app.get('/api', async (req: Request, res: Response) => {
+	res.json({ message: 'Hello, World!' });
+});
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
